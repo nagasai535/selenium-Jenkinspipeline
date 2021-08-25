9f6f086001c41e5121ce027f32dbd56a0f49cf59
nagasai535 <88318249+nagasai535@users.noreply.github.com>
2021-08-25 12:21:59 +0530

    Update Selenium.java

--- comtest/src/main/java/com/test/Selenium.java
+++ comtest/src/main/java/com/test/Selenium.java
@@ -5,10 +5,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Selenium {
 
     public static void main(String[] args) {
-	   
+	   ChromeOptions options = new ChromeOptions();
 	    
         System.setProperty("webdriver.chrome.driver", "comtest/lib/chromedriver.exe"); // <-- Change this path
-        WebDriver driver = new ChromeDriver();
+	    
+	    options.addArguments("--disable-web-security");
+options.addArguments("--ignore-urlfetcher-cert-requests");
+options.addArguments("--disable-renderer-backgrounding");
+options.addArguments("--disable-infobars");
+options.addArguments("--start-maximized");
+options.addArguments("--no-sandbox");
+if (isHeadless) {
+    options.addArguments("--headless");
+}
+	    
+        WebDriver driver = new ChromeDriver(options);
+	    
+	    
         String baseUrl = "https://google.com";
         String expectedTitle = "Google";
         String actualTitle = "";
